bcf347b351bda8bdac0bfb851fa297e93535678d
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-08-02 18:22:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import asyncio
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
-from iot.service import IOTService
+from iot.service import IOTService, run_parallel, run_sequence
 
 
 async def main() -> None:
@@ -20,21 +20,43 @@ async def main() -> None:
 
     # create a few programs
     wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
+        Message(hue_light_id, MessageType.SWITCH_ON, "Lite on"),
+        Message(speaker_id, MessageType.SWITCH_ON, "Speaker on"),
         Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
     ]
 
     sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
+        Message(hue_light_id, MessageType.SWITCH_OFF, "Light off"),
+        Message(speaker_id, MessageType.SWITCH_OFF, "Speaker off"),
         Message(toilet_id, MessageType.FLUSH),
         Message(toilet_id, MessageType.CLEAN),
     ]
 
     # run the programs
-    await service.run_program(wake_up_program)
-    await service.run_program(sleep_program)
+    await run_sequence(
+        service.send_msg(wake_up_program[0]),
+        run_parallel(
+            service.send_msg(wake_up_program[1]),
+            service.send_msg(wake_up_program[2]),
+        ),
+    )
+
+    await run_sequence(
+        service.send_msg(sleep_program[0]),
+        run_parallel(
+            service.send_msg(sleep_program[1]),
+            run_sequence(
+                service.send_msg(sleep_program[2]),
+                service.send_msg(sleep_program[3]),
+            ),
+        ),
+    )
+
+    await run_parallel(
+        service.unregister_device(hue_light_id),
+        service.unregister_device(speaker_id),
+        service.unregister_device(toilet_id),
+    )
 
 
 if __name__ == "__main__":
